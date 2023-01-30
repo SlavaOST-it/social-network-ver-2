2be7c9de70c4406ffd509318e6c9aef67b3a64f8
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2023-01-30 21:18:19 +0300

    feat: common types

---
+++ src/common/types/commonTypes.ts
@@ -0,0 +1,13 @@
+
+
+export enum AppStatus {
+    IDLE = 'idle',
+    LOADING = 'loading',
+    SUCCEEDED = 'succeeded',
+    FAILED = 'failed'
+}
+
+export enum ResultCode  {
+    OK =0,
+    FAILED = 1
+}
