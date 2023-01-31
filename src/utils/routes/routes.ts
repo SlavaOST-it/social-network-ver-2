9cf159a204a3398a58ee6e71aff385c7d12df06f
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2023-01-31 23:06:46 +0300

    feat: add routes

--- src/utils/routes/routes.ts
+++ src/utils/routes/routes.ts
@@ -1,8 +1,8 @@
 export const PATH = {
-    login: 'login',
-    profile: 'profile',
-    users: 'users',
-    messages: 'messages',
-    music: 'music',
-    settings: 'settings'
+    login: '/login',
+    profile: '/profile',
+    users: '/users',
+    messages: '/messages',
+    music: '/music',
+    settings: '/settings'
 }
