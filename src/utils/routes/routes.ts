08f3021aa2803244a22483b0b4ebf3c5a9029d4a
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2023-02-01 23:37:29 +0300

    fix: add new paths

--- src/utils/routes/routes.ts
+++ src/utils/routes/routes.ts
@@ -1,8 +1,10 @@
 export const PATH = {
     login: '/login',
     profile: '/profile',
+    friends: '/friends',
     users: '/users',
     messages: '/messages',
     music: '/music',
+    news: '/news',
     settings: '/settings'
 }
