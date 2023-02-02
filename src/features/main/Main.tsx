46a9ab4978ae6b5743ba09792142f21499d38d7e
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2023-02-02 22:49:19 +0300

    feat: add route

--- src/features/main/Main.tsx
+++ src/features/main/Main.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import s from "./Main.module.scss"
 import {Navigate, Route, Routes} from 'react-router-dom';
 import {PATH} from "../../utils/routes/routes";
 import {Login} from "../login/Login";
+import {ProfilePage} from "../profile/ProfilePage";
 
 
 
 export const Main = () => {
     return (
-        <div className={""}>
+        <div className={s.main}>
             <Routes>
                 <Route path={'/'} element={<Navigate to={PATH.login}/>}/>
                 <Route path={PATH.login} element={<Login/>}/>
+                <Route path={PATH.profile} element={<ProfilePage/>}/>
             </Routes>
         </div>
     );
