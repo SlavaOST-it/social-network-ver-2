13dbf190e213967b794502c904619d62a1d8cd42
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2023-02-05 23:13:12 +0300

    feat: add type for posts

--- src/bll/reducers/reducersTypes/profileReducer-types.ts
+++ src/bll/reducers/reducersTypes/profileReducer-types.ts
@@ -1,21 +1,26 @@
 import {UserProfileType} from "../../../api/apiConfig/typesAPI/profileAPI-types";
-import {setStatusAC, setUserProfileAC} from "../profile-reducer";
+import {addPostAC, setAvatarAC, setStatusAC, setUserProfileAC} from "../profile-reducer";
 
 // ===== Initial State ===== //
 export type ProfilePageType = {
     profile: UserProfileType | null,
+    myAvatar: string | null,
     status: string,
     posts: PostsDataType[]
 }
 
-type PostsDataType = {
+export type PostsDataType = {
     id: number,
-    message: string
+    message: string,
+    likesCount: number,
+    comments: string
 }
 
 
 // ===== Action Type ==== //
 type SetUserProfileAT = ReturnType<typeof setUserProfileAC>
 type SetStatusAT = ReturnType<typeof setStatusAC>
+type SetAvatarAT = ReturnType<typeof setAvatarAC>
+type AddPostAT = ReturnType<typeof addPostAC>
 
-export type UserReducerType = SetUserProfileAT | SetStatusAT
+export type UserReducerType = SetUserProfileAT | SetStatusAT | SetAvatarAT | AddPostAT
