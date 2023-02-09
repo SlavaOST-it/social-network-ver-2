96e4861311869c757b781b5cc94c84ae34fbb379
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2023-02-09 21:13:41 +0300

    feat: add component

---
+++ src/features/posts/addPost/AddPost.tsx
@@ -0,0 +1,39 @@
+import React, {ChangeEvent, useState} from 'react';
+
+import s from "./AddPost.module.scss"
+
+import {useAppDispatch} from "../../../utils/hooks/hooks";
+import {addPostAC} from "../../../bll/reducers/profile-reducer";
+import {ButtonSend} from "../../../common/components/buttonSend/ButtonSend";
+import {AvatarUser} from "../../profile/avatar/AvatarUser";
+
+
+export const AddPost = () => {
+    const dispatch = useAppDispatch()
+
+    const [valueText, setValueText] = useState("")
+
+    const changeTextPost = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setValueText(e.currentTarget.value)
+    }
+
+    const addPostHandler = () => {
+        dispatch(addPostAC({postText: valueText}))
+        setValueText("")
+    }
+
+    return (
+        <div className={s.addPost}>
+            <div className={s.wrapper}>
+                <AvatarUser type={"my"} className={s.avatar}/>
+                <textarea
+                    value={valueText}
+                    onChange={changeTextPost}
+                    placeholder={"Добавить пост..."}
+                    className={s.textArea}
+                />
+                <ButtonSend callBack={addPostHandler}/>
+            </div>
+        </div>
+    );
+};
