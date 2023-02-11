b4874dfa7f16013626c57a921517440b76367e0e
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2023-02-11 17:35:33 +0300

    feat: add component page not found

--- src/common/components/page404/Page404.tsx
+++ src/common/components/page404/Page404.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import s from "./Page404.module.scss"
+import { useNavigate } from 'react-router-dom';
 
+import s from "./Page404.module.scss"
+import page404Logo from "../../../assets/img/404.png"
 
 export const Page404 = () => {
-    return (
-        <div>
 
+    const navigate = useNavigate()
+
+    return (
+        <div className={s.page404}>
+            <img src={page404Logo} alt={'404'}/>
+
+            <p>Страница не найдена</p>
+
+            <button
+                onClick={()=>{navigate(-1)}}
+                className={s.buttonRevers}>назад</button>
         </div>
     );
 };
