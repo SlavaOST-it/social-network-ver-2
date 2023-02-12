5112aaab276fb7988320859a032db488cd5d707f
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2023-02-12 20:38:54 +0300

    fix: fix styles

--- src/common/components/pagination/BasicPagination.tsx
+++ src/common/components/pagination/BasicPagination.tsx
@@ -9,42 +9,61 @@ type BasicPaginationType = {
     currentPage: number,
     portionSize: number
 }
-export const BasicPagination:FC<BasicPaginationType> = ({
-                                                            totalItemsCount,
-                                                            pageSize,
-                                                            onPageChanges,
-    currentPage,
-    portionSize
-                                                        }) => {
+export const BasicPagination: FC<BasicPaginationType> = ({
+                                                             totalItemsCount,
+                                                             pageSize,
+                                                             onPageChanges,
+                                                             currentPage,
+                                                             portionSize
+                                                         }) => {
 
-    const pagesCount = Math.ceil(totalItemsCount / pageSize);
-    const pages = []
-    for (let i = 1; i <= pagesCount; i++) {
-        pages.push(i)
+        const pagesCount = Math.ceil(totalItemsCount / pageSize);
+        const pages = []
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i)
+        }
+
+        const portionCount = Math.ceil(pagesCount / portionSize);
+
+        const [portionNumber, setPortionNumber] = useState(1);
+
+        const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+        const rightPortionPageNumber = (portionNumber * portionSize);
+
+        return (
+            <div className={s.basicPagination}>
+                {portionNumber > 1 &&
+                    <button
+                        onClick={() => {
+                            setPortionNumber(portionNumber - 1)
+                        }}
+                        className={s.btnNextPrev}
+                    >
+                        {'<<'} ПРЕД
+                    </button>}
+
+                {pages
+                    .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+                    .map(p => {
+                        return <span key={p} className={currentPage === p ? s.pageSelected : s.numberPage}
+                                     onClick={() => {
+                                         onPageChanges(p)
+                                     }}
+                        >
+                        {p}
+                    </span>
+                    })}
+
+                {portionCount > portionNumber &&
+                    <button
+                        onClick={() => {
+                            setPortionNumber(portionNumber + 1)
+                        }}
+                        className={s.btnNextPrev}
+                    >
+                        СЛЕД {'>>'}
+                    </button>}
+            </div>
+        );
     }
-
-    const portionCount = Math.ceil(pagesCount / portionSize);
-
-    const [portionNumber, setPortionNumber] = useState(1);
-
-    const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
-    const rightPortionPageNumber = (portionNumber * portionSize);
-
-    return (
-        <div className={s.basicPagination}>
-            {portionNumber > 1 &&
-                <button onClick={()=>{setPortionNumber(portionNumber - 1)}}>PREV</button>}
-
-            {pages
-                .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
-                .map(p => {
-                    return <span key={p} className={currentPage === p ? s.pageSelected : ""}
-                                 onClick={() => {
-                                     onPageChanges(p)
-                                 }}>{p}</span>
-                })}
-            {portionCount > portionNumber &&
-                <button onClick={()=> {setPortionNumber(portionNumber + 1)} }>NEXT</button>}
-        </div>
-    );
-};
+;
