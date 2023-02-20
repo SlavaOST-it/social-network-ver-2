3ef4d3f8af07edf13346653e3191aaa958c4f199
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2023-02-20 23:15:15 +0300

    feat: add props

--- src/common/components/buttonSend/ButtonSend.tsx
+++ src/common/components/buttonSend/ButtonSend.tsx
@@ -4,16 +4,17 @@ import sendButtonLogo from "../../../assets/img/icons/send-03-svgrepo-com.svg";
 
 
 type ButtonSendType = {
-    callBack: () => void
+    id?: number
+    callBack: (id?: number) => void
     className?: string
     disabled?: boolean,
 }
 
-export const ButtonSend: FC<ButtonSendType> = ({callBack, className, disabled}) => {
+export const ButtonSend: FC<ButtonSendType> = ({id, callBack, className, disabled}) => {
     return (
         <button
             className={className ? className : s.sendBtn}
-            onClick={callBack}
+            onClick={()=>callBack(id)}
             disabled={disabled}
         >
             <img src={sendButtonLogo} alt={"send"}/>
