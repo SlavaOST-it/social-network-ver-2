b747499abd0dec63c6181f22f6401a276dd1e180
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2023-02-20 23:16:05 +0300

    feat: add component

--- src/features/dialogs/DialogsPage.tsx
+++ src/features/dialogs/DialogsPage.tsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from "./DialogsPage.module.scss"
+import {useAppSelector} from "../../utils/hooks/hooks";
+import {AddNewMessage} from "./addNewMessage/AddNewMessage";
+import chatLogo from "../../assets/img/icons/chat-svgrepo-com.svg"
 
 
 export const DialogsPage = () => {
+    const dialogsData = useAppSelector(state => state.dialogs.dialogs)
+
+    const [valueDialog, setValueDialog] = useState(0)
+
+    const messagesUser = dialogsData.filter(d => d.dialogId === valueDialog).map(m => m.messages)
+
     return (
-        <div>
+        <div className={s.dialogsPage}>
+
+            <div className={s.dialogsNav}>
+                {dialogsData.map(dialog =>
+                    <div
+                        className={dialog.dialogId === valueDialog ? (`${s.activeDialog} ${s.dialogsNavItem}`) : s.dialogsNavItem}
+                        onClick={() => setValueDialog(dialog.dialogId)}
+                    >
+                        <img src={dialog.avatar} alt={'user avatar'}/>
+                        <p className={s.name}>{dialog.userName}</p>
+                    </div>)}
+            </div>
+
+            <div className={s.dialogs}>
+                <div className={s.messages}>
+                    {valueDialog === 0 &&
+                        <img src={chatLogo} alt={'chat'}/>
+                    }
+
+                    {messagesUser &&
+                        (messagesUser.map(m =>
+                            <div>{m.map(item =>
+                                <p className={item.messageId > 3 ? (`${s.newMessage} ${s.message}`) : s.message}>{item.text}</p>)}
+                            </div>
+                        ))}
+
+                    <div className={s.addNewMessage}>
+                        <AddNewMessage dialogId={valueDialog}/>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div>
+
+            </div>
 
         </div>
     );
