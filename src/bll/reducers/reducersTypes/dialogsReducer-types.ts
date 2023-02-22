d15964435d758b0cc486d2cbd9e12b3c24885057
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2023-02-22 21:02:04 +0300

    fix: fix type

--- src/bll/reducers/reducersTypes/dialogsReducer-types.ts
+++ src/bll/reducers/reducersTypes/dialogsReducer-types.ts
@@ -1,6 +1,11 @@
 import {addNewMessageAC} from "../dialogs-reducer";
 
 
+// ===== Initial State ===== //
+export type DialogsPage = {
+
+}
+
 type AddNewMessageAT = ReturnType<typeof addNewMessageAC>
 
 export type DialogsReducerActionTypes = AddNewMessageAT
