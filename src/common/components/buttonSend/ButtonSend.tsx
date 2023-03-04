e43cd09a3f81926b5aa9f65139b1b035d4f27f7f
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2023-03-04 20:43:49 +0300

    feat: add type

--- src/common/components/buttonSend/ButtonSend.tsx
+++ src/common/components/buttonSend/ButtonSend.tsx
@@ -5,14 +5,16 @@ import sendButtonLogo from "../../../assets/img/icons/send-03-svgrepo-com.svg";
 
 type ButtonSendType = {
     id?: number
+    type?: "button" | "submit" | "reset" | undefined
     callBack: (id?: number) => void
     className?: string
     disabled?: boolean,
 }
 
-export const ButtonSend: FC<ButtonSendType> = ({id, callBack, className, disabled}) => {
+export const ButtonSend: FC<ButtonSendType> = ({id, type,callBack, className, disabled}) => {
     return (
         <button
+            type={type}
             className={className ? className : s.sendBtn}
             onClick={()=>callBack(id)}
             disabled={disabled}
