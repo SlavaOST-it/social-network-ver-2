18fd0e9f7ce25438d97aaf98aeedda6d1c7c0a81
SlavaOST <slavaost-it@mail.ru>
2023-03-15 19:29:12 +0300

    feat: add .env

--- src/api/apiConfig/instance.ts
+++ src/api/apiConfig/instance.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const instance = axios.create({
     withCredentials: true,
-    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    baseURL: 'https://social-network.samuraijs.com/api/1.0',
     headers: {
-        "API-KEY": "dd070108-da2d-47ec-bd5a-e22f291be6bf"
+        "API-KEY": process.env.REACT_APP_API_KEY
     }
 });
