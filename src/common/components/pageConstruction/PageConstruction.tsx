f4ed683613d569ebcf42ceb44e718f61f2c7ccf7
SlavaOST <slavaost-it@mail.ru>
2023-03-25 13:27:43 +0300

    feat: add loggedIn

--- src/common/components/pageConstruction/PageConstruction.tsx
+++ src/common/components/pageConstruction/PageConstruction.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {Navigate} from "react-router-dom";
 
 import s from "./PageConstruction.module.scss"
+
 import reconstructionLogo from "../../../assets/img/reconstruktion.png"
+
+import {PATH} from "../../../utils/routes/routes";
+import {useAppSelector} from "../../../utils/hooks/hooks";
 
 
 export const PageConstruction = () => {
+    const loggedIn = useAppSelector(state => state.auth.loggedIn)
+
+    if (!loggedIn) {
+        return <Navigate to={PATH.login}/>
+    }
+
     return (
         <div className={s.pageConstruction}>
             <img src={reconstructionLogo} alt={"reconstruction page"}/>
